3ce2adf5b157cafee644b6a6db7703e153192277
Ken Code Developer <kenichidevelopment@gmail.com>
2026-07-06 09:01:45 +0700

    Create RotatingDisc.jsx

---
+++ src/components/RotatingDisc.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { Text } from 'ink';
+import useInterval from '../hooks/useInterval.js';
+
+const frames = ['◷', '◶', '◵', '◴'];
+
+export default function RotatingDisc() {
+  const [frameIndex, setFrameIndex] = useState(0);
+
+  useInterval(() => {
+    setFrameIndex((prev) => (prev + 1) % frames.length);
+  }, 80); // ~12.5 FPS
+
+  return (
+    <Text>
+      💿 {frames[frameIndex]}
+    </Text>
+  );
+}
