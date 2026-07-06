d2a410ef26d68e3c8e2c18d0ee033e2dc9f30e62
Ken Code Developer <kenichidevelopment@gmail.com>
2026-07-06 08:58:24 +0700

    ehee

---
+++ songs/shapeofmyheart/data/song.js
@@ -0,0 +1,122 @@
+export default {
+  title: "Shape Of My Heart",
+  artist: "Backstreet Boys",
+  album: "Black & Blue",
+  duration: 94,
+  audioFile: "../shapeofmyheart.m4a",
+  initialDelay: 0,
+  startupDelay: 0,        // fade in awal
+  fadeOutDelay: 1500,        // fade out akhir
+
+  lyrics: [
+    {
+      text: "I'm here with my confession",
+      typingSpeed: 80,
+      nextDelay: 2200
+    },
+    {
+      text: "Got nothing to hide no more",
+      typingSpeed: 80,
+      nextDelay: 1300
+    },
+    {
+      text: "I don't know where to start",
+      typingSpeed: 80,
+      nextDelay: 1900
+    },
+    {
+      text: "But to show you the shape of my heart",
+      typingSpeed: 80,
+      nextDelay: 0
+    },
+    // Jeda bait (baris kosong)
+    {
+      text: "",
+      typingSpeed: 0,
+      nextDelay: 7500
+    },
+    {
+      text: "I'm lookin' back on things I've done",
+      typingSpeed: 80,
+      nextDelay: 1900
+    },
+    {
+      text: "I never wanna play the same old part",
+      typingSpeed: 80,
+      nextDelay: 1900
+    },
+    {
+      text: "I'll keep you in the dark",
+      typingSpeed: 80,
+      nextDelay: 1900
+    },
+    {
+      text: "Now let me show you the shape of my heart",
+      typingSpeed: 80,
+      nextDelay: 0
+    },
+    // Jeda bait
+    {
+      text: "",
+      typingSpeed: 0,
+      nextDelay: 800
+    },
+    {
+      text: "Looking back on the things I've done",
+      typingSpeed: 70,
+      nextDelay: 1000 //done
+    },
+    {
+      text: "I was trying to be someone (trying to be someone)",
+      typingSpeed: 50,
+      nextDelay: 3000 //done
+    },
+    {
+      text: "I played my part, kept you in the dark",
+      typingSpeed: 50,
+      nextDelay: 1700 //done
+    },
+    {
+      text: "Now let me show you the shape of my heart",
+      typingSpeed: 85,
+      nextDelay: 2000
+    },
+    {
+      text: "Looking back on the things I've done",
+      typingSpeed: 70,
+      nextDelay: 1200
+    },
+    {
+      text: "I was trying to be someone",
+      typingSpeed: 80,
+      nextDelay: 2000
+    },
+    {
+      text: "I played my part, kept you in the dark",
+      typingSpeed: 70,
+      nextDelay: 2800
+    },
+    {
+      text: "Now let me show you the shape of",
+      typingSpeed: 70,
+      nextDelay: 0
+    },
+    // Jeda bait
+    {
+      text: "",
+      typingSpeed: 0,
+      nextDelay: 3500
+    },
+    {
+      text: "Show you the shape of my heart",
+      typingSpeed: 70,
+      nextDelay: 1600
+    },
+  ],
+
+  footer: [
+    "Dadaahh eheee",
+    "Made with ❤️",
+    "Keniii (RevsCloud) - 2026"
+  ]
+}
